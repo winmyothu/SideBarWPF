fe50d129fe3c0797f157b66694f26a2bc6ebbc69
Win Myo Thu <winmyothu@myanmareasenet.com>
2023-01-23 00:39:34 +0630

    init project

---
+++ MMDictionary/ViewModel/NavigationVM.cs
@@ -0,0 +1,34 @@
+﻿using MMDictionary.Ultilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MMDictionary.ViewModel
+{
+    class NavigationVM : ViewModelBase
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+        public ICommand HomeCommand { get; set; }
+        public ICommand AboutCommand { get; set; }
+
+        private void Home(object obj)=> CurrentView = new HomeVM();
+        private void About(object obj)=> CurrentView = new AboutVM();
+
+        public NavigationVM()
+        {
+            HomeCommand = new RelayCommand(Home);
+            AboutCommand = new RelayCommand(About);
+
+            //Startup Page
+            CurrentView = new HomeVM();
+        }
+    }
+}
